15a470ffc9b1aad867016f391130b8c908572327
unknown <daidaixb@qq.com>
2016-10-06 21:00:35 +0800

    Master change little

--- 12306/my12306.py
+++ 12306/my12306.py
@@ -24,8 +24,6 @@ try:
 except ImportError:
     import pickle
 from docopt import docopt
-<<<<<<< HEAD:12306/my12306.py
-# from datetime import date
 from functions import exit_after_echo, translate_date, TrainsCollection, generate_stations
 
 
